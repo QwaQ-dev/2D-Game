aff6df71b20b2cba9a33c549b4c45adc7e8ef392
QwaQ <qwaq.dev@gmail.com>
2021-01-29 18:47:57 +0500

    Added a first aid kit with scripts for it

---
+++ Assets/Scripts/Aid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aid : MonoBehaviour
+{
+    public int bonusHealth;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Health health = col.gameObject.GetComponent<Health>();
+            health.SetHealth(bonusHealth);
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Platfrom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platfrom : MonoBehaviour
+{
+    public float force = 10f;
+    public GroundDetection groundDetection;
+    public Rigidbody2D rb;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.relativeVelocity.y < 0)
+            Player.instance.rb.velocity = Vector2.up * force;
+    }
+}
