6c9f2f3d0fdbb67708438dc9b6fdb542eff5b0e4
QwaQ <qwaq.dev@gmail.com>
2021-01-29 17:26:02 +0500

    First demo version!

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject leftBorder;
+    public GameObject rightBorder;
+    public Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
+    public bool isRightDirection;
+
+    public float speed;
+    private void Update()
     {
-        
+        if (isRightDirection)
+        {
+            rb.velocity = Vector2.right * speed;
+            if (transform.position.x > rightBorder.transform.position.x)
+                isRightDirection = !isRightDirection;
+        }
+        else
+        {
+            rb.velocity = Vector2.left * speed;
+            if (transform.position.x < leftBorder.transform.position.x)
+                isRightDirection = !isRightDirection;
+        }
     }
 }
