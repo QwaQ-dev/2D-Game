47c2499aa910a63a2fd78f9b3304d143bb386842
QwaQ <qwaq.dev@gmail.com>
2021-02-06 22:28:09 +0500

    Created a game!

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -9,18 +9,21 @@ public class MovingPlatform : MonoBehaviour
     public Rigidbody2D rb;
 
     public bool isRightDirection;
+    public SpriteRenderer spriteRenderer;
 
     public float speed;
     private void Update()
     {
         if (isRightDirection)
         {
+            spriteRenderer.flipX = true;
             rb.velocity = Vector2.right * speed;
             if (transform.position.x > rightBorder.transform.position.x)
                 isRightDirection = !isRightDirection;
         }
         else
         {
+            spriteRenderer.flipX = false;
             rb.velocity = Vector2.left * speed;
             if (transform.position.x < leftBorder.transform.position.x)
                 isRightDirection = !isRightDirection;
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public int coinsCount;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Coin"))
+        {
+            coinsCount++;
+            Debug.Log("количество монет = " + coinsCount);
+            Destroy(col.gameObject);
+        }
+    }
+}
